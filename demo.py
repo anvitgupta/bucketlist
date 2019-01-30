97a08df907802da54b7acaa864b4966cbed53a47
yunhua98 <yunhua98@gmail.com>
2019-01-30 14:02:26 -0600

    Add initial demo code with set up Firebase

---
+++ demo.py
@@ -0,0 +1,30 @@
+from flask import Flask
+import pyrebase
+
+app = Flask(__name__)
+
+@app.route('/')
+def display_users():
+    output = []
+    config = {
+            "apiKey": "[redacted]",
+            "authDomain": "bucket-list-swe.firebaseapp.com",
+            "databaseURL": "https://bucket-list-swe.firebaseio.com",
+            "storageBucket": "bucket-list-swe.appspot.com"
+            }
+    
+    firebase = pyrebase.initialize_app(config)
+    
+    db = firebase.database()
+    
+    bucket_list_items = db.child("bucket_list_items").get()
+    
+    output.append("[Bucket List]")
+    for item in bucket_list_items.each():
+        output.append(item.val()["description"] + " - added by: ")
+        added = []
+        for user in item.val()["added_by"]:
+            added.append(user)
+        output[-1] += ", ".join(added)
+
+    return "<br/>".join(output)
