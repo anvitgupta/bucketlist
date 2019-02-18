3f36e021d74caec5b152f6b6dfef50dcc18328b4
cavanbriody <cavanbriody@gmail.com>
2019-02-18 13:44:38 -0600

    notes and initial project

---
+++ BucketListIOS/BucketListIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BucketListIOS
+//
+//  Created by Cavan Briody on 2/10/19.
+//  Copyright © 2019 Cavan Briody. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+
+
+}
+
---
+++ BucketListIOS/Pods/Target Support Files/Pods-BucketListIOS/Pods-BucketListIOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BucketListIOS : NSObject
+@end
+@implementation PodsDummy_Pods_BucketListIOS
+@end
