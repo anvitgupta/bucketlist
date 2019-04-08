bd74600468a1ff564c360db624d1418a84581a0c
anvitgupta <anvit.gupta@vanderbilt.edu>
2019-04-08 15:45:21 -0500

    Updated database in user list, post list, and activity list

--- public/scripts/makePost.js
+++ public/scripts/makePost.js
@@ -53,10 +53,68 @@ function sendData() {
         date : postDate,
         description: postDescription,
         score: 0,
-        pic: loadImage,
-        users_added: [userName],
         timestamp: Date.now(),
-        key: newPostKey
+        key: newPostKey,
+        completed: false
+    }
+
+    console.log(postData);
+
+    var updates = {};
+    updates['/bucket_list_items/' + newPostKey] = postData;
+
+    firebase.database().ref().update(updates)
+    .then(function(){
+        console.log('Synchronization succeeded');
+        window.location.replace("profile.html");
+    })
+    .catch(function(error) {
+        console.log('Synchronization failed');
+    });
+}
+
+function signOut() {
+    firebase.auth().signOut()
+    .then(function() {
+        window.location.replace("login.html");
+    })
+    .catch(function(error) {
+        console.log(error)
+  });
+}
+
+function setValue(id,newvalue) {
+    var s= document.getElementById(id);
+    s.value = newvalue;
+} 
+
+function sleep(delay) {
+    var start = new Date().getTime();
+    while (new Date().getTime() < start + delay);
+  }
+
+  function sendData() {
+
+    user = firebase.auth().currentUser;
+    const postTitle = document.getElementById('activity-input').value.toString();
+    const postDate = document.getElementById('date-input').value.toString();
+    const postDescription = document.getElementById('description-input').value.toString();
+    const userName = user.displayName.toString().replace(/\s/g, '');
+
+    var reader  = new FileReader();
+    console.log(document.getElementById('file-input').files[0]);
+
+    var newPostKey = firebase.database().ref().child('bucket_list_items').push().key;
+
+    var postData = {
+        title: postTitle,
+        creator: userName,
+        date : postDate,
+        description: postDescription,
+        score: 0,
+        timestamp: Date.now(),
+        key: newPostKey,
+        completed: false
     }
 
     console.log(postData);
