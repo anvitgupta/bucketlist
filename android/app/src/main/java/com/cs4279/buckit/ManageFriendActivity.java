8c5a7605e85a4c80bc7811289be32e7f385f8c3d
Harsha Vankayalapati <vhrsha11@gmail.com>
2019-04-15 14:57:51 -0500

    Added some friend/follower improvements

--- android/app/src/main/java/com/cs4279/buckit/ManageFriendActivity.java
+++ android/app/src/main/java/com/cs4279/buckit/ManageFriendActivity.java
@@ -4,8 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.HashSet;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class ManageFriendActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
@@ -54,7 +70,7 @@ public class ManageFriendActivity extends AppCompatActivity {
 
                     itemsList.add(newItem);
                 }
-
+                // FirebaseDatabase.child()
                 mAdapter.notifyDataSetChanged();
             }
 
