21d24d37aa051b6b819d05db7bef1d96d7497432
yunhua98 <yunhua98@gmail.com>
2019-04-18 21:27:38 -0500

    Fix image loading issues

--- android/app/src/main/java/com/cs4279/buckit/ViewImageActivity.java
+++ android/app/src/main/java/com/cs4279/buckit/ViewImageActivity.java
@@ -5,14 +5,18 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class ViewImageActivity extends AppCompatActivity  {
+    private FirebaseAuth firebaseAuth;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_image);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         String key = getIntent().getStringExtra("key");
 
